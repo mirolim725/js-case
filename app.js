b3ba958aa42b085d49378404410149b23b34cdd5
Mirolimdev <mirolimsherboboyev@gmail.com>
2024-04-11 15:04:37 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,747 @@
+'use strict'
+
+
+// CASE-1
+
+// let dayName = 1;
+// switch(dayName){
+//     case 1:
+//         dayName = 'Dushanba';
+//         break;
+//     case 2:
+//         dayName = 'Seshanba';
+//         break;
+//     case 3:
+//         dayName = 'Chorshanba';
+//         break;
+//     case 4:
+//         dayName = 'Payshanba';
+//         break;
+//     case 5:
+//         dayName = 'Juma';
+//         break;
+//     case 6:
+//         dayName = 'Shanba';
+//         break;
+//     case 7:
+//         dayName = 'Yakshanba';
+//         break;
+//     default:
+//         dayName = 'Mavjud bo`lmagan hafta kuni';
+// }
+
+// console.log(dayName);
+
+
+
+// CASE-2
+
+
+// let mark = 4;
+// switch(mark){
+//     case 1:
+//         mark = 'Yomon';
+//         break;
+//     case 2:
+//         mark = 'Qoniqarsiz';
+//         break;
+//     case 3:
+//         mark = 'Qoniqarli';
+//         break;
+//     case 4:
+//         mark = 'Yaxshi';
+//         break;
+//     case 5:
+//         mark = 'A`lo';
+//         break;
+//     default:
+//         mark = 'Xato';
+// }
+
+// console.log(mark);
+
+
+
+
+
+// CASE-3
+
+
+
+
+// let month = 1;
+// switch(month){
+//     case 1:
+//         month = 'Qish';
+//         break;
+//     case 2 :
+//         month = 'Qish';
+//         break;
+//     case 12 :
+//         month = 'Qish';
+//         break;
+//     case 3 :
+//         month = 'Bahor';
+//         break;
+//     case 4 :
+//         month = 'Bahor';
+//         break;
+//     case 5 :
+//         month = 'Bahor';
+//         break;
+//     case 6 :
+//         month = 'Yoz';
+//         break;
+//     case 7 :
+//         month = 'Yoz';
+//         break;
+//     case 8 :
+//         month = 'Yoz';
+//         break;
+//     case 9 :
+//         month = 'Kuz';
+//         break;
+//     case 10 :
+//         month = 'Kuz';
+//         break;
+//     case 11 :
+//         month = 'Kuz';
+//         break;
+//     default:
+//         month = 'Bunday oy yo`q';
+// }
+
+// console.log(month);
+
+
+// CASE-4
+
+
+
+// let month = 1;
+// switch(month){
+//     case 1:
+//         month = 'Yanvar 31 kundan iborat';
+//         break;
+//     case 2 :
+//         month = 'Fevral 28 kundan iborat';
+//         break;
+//     case 12 :
+//         month = 'Dekabr 31 kundan iborat';
+//         break;
+//     case 3 :
+//         month = 'Mart 31 kundan iborat';
+//         break;
+//     case 4 :
+//         month = 'Aprel 30 kundan iborat';
+//         break;
+//     case 5 :
+//         month = 'May 31 kundan iborat';
+//         break;
+//     case 6 :
+//         month = 'Iyun 30 kundan iborat';
+//         break;
+//     case 7 :
+//         month = 'Iyul 31 kundan iborat';
+//         break;
+//     case 8 :
+//         month = 'Avgust 31 kundan iborat';
+//         break;
+//     case 9 :
+//         month = 'Sentyabr 30 kundan iborat';
+//         break;
+//     case 10 :
+//         month = 'Oktyabr 31 kundan iborat';
+//         break;
+//     case 11 :
+//         month = 'Noyabr 30 kundan iborat';
+//         break;
+//     default:
+//         month = 'Yanvar 31 kundan iborat';
+// }
+
+// console.log(month);
+
+
+
+
+// CASE-5
+
+
+
+// let a = 4 ,  b = 5 , func = 1;
+// switch(func){
+//     case 1:
+//         console.log(` ${a} ni ${b}ga qo'shsak` ,  a+b , ` bo'ladi` );
+//         break;
+//     case 2:
+//         console.log(` ${a} dan ${b}ni ayirsak` ,  a-b , ` bo'ladi` );
+//         break;
+//     case 3:
+//         console.log(` ${a} ni ${b}ga bo'lsak ` ,  a/b , ` bo'ladi` );
+//         break;
+//     case 4:
+//         console.log(` ${a} ni ${b}ga ko'paytirsak` ,  a*b , ` bo'ladi` );
+//         break;
+//     default:
+//         func = 'Mavjud bo`lmagan amal';
+// }
+
+
+
+
+
+// CASE-6
+
+
+
+
+// let amount = 4 ,  measurement = 1 ;
+// switch(measurement){
+//     case 1:
+//         console.log(` ${amount} detsimetr ${amount/10} metr bo'ladi`);
+//         break;
+//     case 2:
+//         console.log(` ${amount} kilometr ${amount*1000} metr bo'ladi`);
+//         break;
+//     case 3:
+//         console.log(` ${amount} metr ${amount} metr bo'ladi`);
+//         break;
+//     case 4:
+//         console.log(` ${amount} millimetr ${amount/1000} metr bo'ladi`);
+//         break;
+//     case 5:
+//         console.log(` ${amount} santimetr ${amount/100} metr bo'ladi`);
+//         break;
+//     default:
+//         measurement = 'Mavjud bo`lmagan amal';
+// }
+
+
+
+// CASE-7
+
+
+
+
+// let amount = 4 ,  measurement = 5 ;
+// switch(measurement){
+//     case 1:
+//         console.log(` ${amount} kilogramm ${amount} kilogramm bo'ladi`);
+//         break;
+//     case 2:
+//         console.log(` ${amount} milligram ${amount*1000000} kilogram bo'ladi`);
+//         break;
+//     case 3:
+//         console.log(` ${amount} gramm ${amount/1000} kilogram bo'ladi`);
+//         break;
+//     case 4:
+//         console.log(` ${amount} tonna ${amount*1000} kilogram bo'ladi`);
+//         break;
+//     case 5:
+//         console.log(` ${amount} sentner ${amount*100} kilogram bo'ladi`);
+//         break;
+//     default:
+//         measurement = 'Mavjud bo`lmagan amal';
+// }
+
+
+
+
+// CASE-8
+
+
+
+
+
+// let day = 29;
+// let month = 2;
+
+// let daysInMonth;
+
+// switch (month) {
+//     case 1: 
+//         month ='Yanvar';
+//         daysInMonth = 31;
+//         break;
+//     case 3:
+//         month ='Mart';
+//         daysInMonth = 31;
+//         break;
+//     case 5:
+//         month ='May';
+//         daysInMonth = 31;
+//         break;
+//     case 7:
+//         month ='Iyul';
+//         daysInMonth = 31;
+//         break;
+//     case 8:
+//         month ='Avgust';
+//         daysInMonth = 31;
+//         break;
+//     case 10:
+//         month ='Oktyabr';
+//         daysInMonth = 31;
+//         break;
+//     case 12:
+//         month ='Dekabr';
+//         daysInMonth = 31;
+//         break;
+//     case 4:
+//         month ='Aprel';
+//         daysInMonth = 30;
+//         break;
+//     case 6:
+//         month ='Iyun';
+//         daysInMonth = 30;
+//         break;
+//     case 9:
+//         month ='Sentyabr';
+//         daysInMonth = 30;
+//         break;
+//     case 11:
+//         month ='Noyabr';
+//         daysInMonth = 30;
+//         break;
+
+//     case 2:
+//         month = 'Fevral'
+//         daysInMonth = 28; 
+//         break;
+//     default:
+//         daysInMonth = null;
+// }
+
+// if (day <= daysInMonth && daysInMonth !== null) {
+//     console.log(`${day}chi ${month}`);
+// } else {
+//     console.log("Xato sana!");
+// }
+
+
+
+
+// CASE-9
+
+
+
+
+
+// let day = 28;
+// let month = 2;
+
+// let daysInMonth;
+
+// switch (month) {
+//     case 1: 
+//         month ='Yanvar';
+//         daysInMonth = 31;
+//         break;
+//     case 3:
+//         month ='Mart';
+//         daysInMonth = 31;
+//         break;
+//     case 5:
+//         month ='May';
+//         daysInMonth = 31;
+//         break;
+//     case 7:
+//         month ='Iyul';
+//         daysInMonth = 31;
+//         break;
+//     case 8:
+//         month ='Avgust';
+//         daysInMonth = 31;
+//         break;
+//     case 10:
+//         month ='Oktyabr';
+//         daysInMonth = 31;
+//         break;
+//     case 12:
+//         month ='Dekabr';
+//         daysInMonth = 31;
+//         break;
+//     case 4:
+//         month ='Aprel';
+//         daysInMonth = 30;
+//         break;
+//     case 6:
+//         month ='Iyun';
+//         daysInMonth = 30;
+//         break;
+//     case 9:
+//         month ='Sentyabr';
+//         daysInMonth = 30;
+//         break;
+//     case 11:
+//         month ='Noyabr';
+//         daysInMonth = 30;
+//         break;
+
+//     case 2:
+//         month = 'Fevral'
+//         daysInMonth = 28; 
+//         break;
+//     default:
+//         daysInMonth = null;
+// }
+
+// if (day <= daysInMonth && daysInMonth !== null) {
+//     day = day + 1
+//     if (day <= daysInMonth && daysInMonth !== null){
+//         console.log(`${day}chi ${month}`);
+//     }
+//     else{
+//         console.log(` 1 chi ${month}`);
+//     }
+// } else {
+//     console.log("Xato sana!");
+// }
+
+
+
+
+
+
+// CASE-10
+
+
+
+
+// let y = 's' , k = 0;
+
+
+// if (k === 0) {
+//     switch (y) {
+//         case 's':
+//             y = 'Shimol';
+//             break;
+        
+//         case 'j':
+//             y = 'Janub';
+//             break;
+        
+//         case 'q':
+//             y = 'Sharq';
+//             break;
+        
+//         case 'g':
+//             y = 'G`arb';
+//             break;
+        
+//         default:
+//             y = 'yo`nalish xato';
+//             break;
+//     }
+// }  else if (k === 1) {
+//     switch (y) {
+//         case 's':
+//             y = 'G`arb';
+//             break;
+        
+//         case 'j':
+//             y = 'Sharq';
+//             break;
+        
+//         case 'q':
+//             y = 'Shimol';
+//             break;
+        
+//         case 'g':
+//             y = 'Janub';
+//             break;
+        
+//         default:
+//             y = 'yo`nalish xato';
+//             break;
+//     }
+// }else if (k === 2) {
+//     switch (y) {
+//         case 's':
+//             y = 'Sharq';
+//             break;
+        
+//         case 'j':
+//             y = 'G`arb';
+//             break;
+        
+//         case 'q':
+//             y = 'Janub';
+//             break;
+        
+//         case 'g':
+//             y = 'Shimol';
+//             break;
+        
+//         default:
+//             y = 'yo`nalish xato';
+//             break;
+//     }
+// } else{
+//     console.log('Xato komanda');
+// }
+
+// console.log(y);
+
+
+
+
+
+
+// CASE-11
+
+
+
+
+
+
+// let c = 's' , k = 0;
+
+
+// if (k === 2) {
+//     switch (c) {
+//         case 's':
+//             c = 'Janub';
+//             break;
+        
+//         case 'j':
+//             c = 'Shimol;';
+//             break;
+        
+//         case 'q':
+//             c = 'G`arb';
+//             break;
+        
+//         case 'g':
+//             c = 'Sharq';
+//             break;
+        
+//         default:
+//             c = 'yo`nalish xato';
+//             break;
+//     }
+// }  else if (k === 1) {
+//     switch (c) {
+//         case 's':
+//             c = 'G`arb';
+//             break;
+        
+//         case 'j':
+//             c = 'Sharq';
+//             break;
+        
+//         case 'q':
+//             c = 'Shimol';
+//             break;
+        
+//         case 'g':
+//             c = 'Janub';
+//             break;
+        
+//         default:
+//             c = 'yo`nalish xato';
+//             break;
+//     }
+// }else if (k === 0) {
+//     switch (c) {
+//         case 's':
+//             c = 'Sharq';
+//             break;
+        
+//         case 'j':
+//             c = 'G`arb';
+//             break;
+        
+//         case 'q':
+//             c = 'Janub';
+//             break;
+        
+//         case 'g':
+//             c = 'Shimol';
+//             break;
+        
+//         default:
+//             c = 'yo`nalish xato';
+//             break;
+//     }
+// } else{
+//     console.log('Xato komanda');
+// }
+
+// console.log(c);
+
+
+
+
+
+// CASE-12
+
+
+// const p = 3.14;
+
+// let R, D, L, S;
+
+// let element = 2;
+
+// switch (element) {
+//     case 1: 
+//         R = 1;
+//         console.log( R);
+//         D = 2 * R;
+//         console.log( D);
+//         L = 2 * p - R;
+//         console.log(L);
+//         S = p * R ** 2;
+//         console.log(S);
+//         break;
+//     case 2:
+//         D = 1;
+//         console.log(D);
+//         R = D / 2;
+//         console.log(R);
+//         L = 2 * p - R;
+//         console.log(L);
+//         S = p * R ** 2;
+//         console.log(S);
+//         break;
+//     case 3:
+//         L = 1;
+//         console.log(L);
+//         R = (2 * p - L) / 2;
+//         console.log(R);
+//         D = 2 * R;
+//         console.log(D);
+//         S = p * R ** 2;
+//         console.log(S);
+//         break;
+//     case 4: 
+//         S = 1;
+//         console.log( S);
+//         R = Math.sqrt(S / p);
+//         console.log( R);
+//         D = 2 * R;
+//         console.log( D);
+//         L = 2 * p - R;
+//         console.log(L);
+//         break;
+//     default:
+//         console.log("Xato element nomeri");
+// }
+
+
+// CASE-13
+
+
+
+// let a, c, h, S;
+
+// let element = 1;
+
+// switch (element) {
+//     case 1: 
+//         a = 1;
+//         console.log( a);
+//         c = a * Math.sqrt(2);
+//         console.log( c);
+//         h = c / 2;
+//         console.log(h);
+//         S = (c * h) / 2;
+//         console.log(S);
+//         break;
+//     case 2:
+//         c = 1;
+//         console.log(c);
+//         a = c / Math.sqrt(2);
+//         console.log(a);
+//         h = c / 2;
+//         console.log(h);
+//         S = (c * h) / 2;
+//         console.log(S);
+//         break;
+//     case 3:
+//         h = 1;
+//         console.log(h);
+//         a = (2 * h) / Math.sqrt(2)
+//         console.log(a);
+//         c = a * Math.sqrt(2);
+//         console.log(c);
+//         S = (c * h) / 2;
+//         console.log(S);
+//         break;
+//     case 4: 
+//         S = 1;
+//         console.log( S);
+//         a = Math.sqrt(4 * S);
+//         console.log( a);
+//         c = a * Math.sqrt(2);
+//         console.log( c);
+//         h = c / 2;
+//         console.log(h);
+//         break;
+//     default:
+//         console.log("Xato element nomeri");
+// }
+
+
+
+
+
+// CASE-14
+
+
+
+
+
+
+let a, c, h, S;
+
+let element = 1;
+
+switch (element) {
+    case 1: 
+        a = 1;
+        console.log( a);
+        c = a * Math.sqrt(2);
+        console.log( c);
+        h = c / 2;
+        console.log(h);
+        S = (c * h) / 2;
+        console.log(S);
+        break;
+    case 2:
+        c = 1;
+        console.log(c);
+        a = c / Math.sqrt(2);
+        console.log(a);
+        h = c / 2;
+        console.log(h);
+        S = (c * h) / 2;
+        console.log(S);
+        break;
+    case 3:
+        h = 1;
+        console.log(h);
+        a = (2 * h) / Math.sqrt(2)
+        console.log(a);
+        c = a * Math.sqrt(2);
+        console.log(c);
+        S = (c * h) / 2;
+        console.log(S);
+        break;
+    case 4: 
+        S = 1;
+        console.log( S);
+        a = Math.sqrt(4 * S);
+        console.log( a);
+        c = a * Math.sqrt(2);
+        console.log( c);
+        h = c / 2;
+        console.log(h);
+        break;
+    default:
+        console.log("Xato element nomeri");
+}
+
+
+
